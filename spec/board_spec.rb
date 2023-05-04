85b382810f10b5bce9b9bc3b38c6436b2da4fa0c
Jacob Sanders <sanders.jms@gmail.com>
2023-05-04 05:04:47 -0600

    refactored tests for #board_full

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,25 +6,25 @@ describe Board do
   describe "#board_full?" do
 
     context 'when the board is full' do
-      subject(:board) { described_class.new(Array.new(9, 'x')) }
+      subject(:full_board) { described_class.new(Array.new(9, 'x')) }
       it 'returns true' do
-        expect(board.board_full?).to eq(true)
+        expect(full_board).to be_board_full
       end
     end
 
     context 'when the board is not full' do
-      subject(:board) { described_class.new }
+      subject(:empty_board) { described_class.new }
       it 'returns false' do
-        expect(board.board_full?).to eq(false)
+        expect(empty_board).to_not be_board_full
       end
     end
 
     context 'when the board is one away from full' do
-      subject(:board) { described_class.new(Array.new(9, 'x')) }
+      subject(:partial_board) { described_class.new(Array.new(9, 'x')) }
       it 'returns false' do
-        my_board = board.instance_variable_get(:@board)
+        my_board = partial_board.instance_variable_get(:@board)
         my_board[1] = 1
-        verified_result = board.board_full?
+        verified_result = partial_board.board_full?
         expect(verified_result).to eq(false)
       end
     end
