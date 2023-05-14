3ee0b45458bf541682b368d9e7fdc6f4f8ea0168
Jacob Sanders <sanders.jms@gmail.com>
2023-05-14 13:12:57 -0600

    finsihed tests for game_over, check win, check tie, tie message

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -17,22 +17,30 @@ describe TicTacToe do
 
   describe '#switch_player' do
 
+    before do
+      game.instance_variable_set(:@players, [player1, player2])
+    end
+
     context 'when the current player is player 1' do
 
+      before do
+        game.instance_variable_set(:@current_player, player1)
+      end
+
       it 'switches to player 2' do
-        players = [player1, player2]
-        current_player = player1
-        new_current = game.switch_player(current_player, players)
+        new_current = game.switch_player
         expect(new_current).to be(player2)
       end
     end
 
     context 'when the current player is player 2' do
 
+      before do
+        game.instance_variable_set(:@current_player, player2)
+      end
+
       it 'switches to player 1' do
-        players = [player1, player2]
-        current_player = player2
-        new_current = game.switch_player(current_player, players)
+        new_current = game.switch_player
         expect(new_current).to be(player1)
       end
     end
@@ -40,11 +48,137 @@ describe TicTacToe do
     context 'when the current player has not been defined yet' do
 
       it 'switches to player 1' do
-        players = [player1, player2]
-        current_player = nil
-        new_current = game.switch_player(current_player, players)
+        new_current = game.switch_player
         expect(new_current).to be(player1)
       end
+    end
+  end
+
+  describe '#game_won?' do
+
+    context 'when there are three values in a row' do
+
+      subject(:game_full_board) { described_class.new(Board.new(Array.new(9, 'X')), player1) }
+
+      it 'the game is won' do
+        expect(game_full_board).to be_game_won
+        game_full_board.game_won?
+      end
+    end
+
+    context 'when there are not three values in a row' do
+      
+      subject(:game_continues) { described_class.new(board_continues, player1) }
+      let(:board_continues) { double(Board.new) }
+
+      before do
+        allow(board_continues).to receive(:board).and_return([1, 2, 3, 'X', 'Y', 'X', 7, 8, 9])
+      end
+
+      it 'the game continues' do
+        expect(game_continues).to_not be_game_won
+        game_continues.game_won?
+      end
+    end
+  end
+
+  describe '#check_win' do
+
+    before do
+      game.instance_variable_set(:@current_player, player1)
+      allow(game).to receive(:win_message)
+    end
+
+    context 'when the game is not won' do
+
+      before do
+        allow(game).to receive(:game_won?).and_return(false)
+      end
+
+      it 'keeps the state of game over as false' do
+        result = false
+        expect(game.instance_variable_get(:@game_over)).to be(result)
+        game.check_win
+      end
+    end
+
+    context 'when the game is won' do
+
+      before do 
+        allow(game).to receive(:game_won?).and_return(true)
+      end
+
+      it 'changes the state of game over to true' do
+        expect { game.check_win }.to change { game.instance_variable_get(:@game_over) }.to(true)
+      end
+    end
+  end
+
+  describe '#check_tie' do
+
+    subject(:cats_game) { described_class.new(game_board) }
+    let(:game_board) { double Board.new }
+
+    context 'when the board is full and game is not won' do
+
+      before do
+        allow(game_board).to receive(:board_full?).and_return(true)
+        allow(cats_game).to receive(:game_won?).and_return(false)
+        allow(game_board).to receive(:display_board)
+        allow(cats_game).to receive(:tie_message)
+      end
+
+      it 'changes the state of game over to true' do
+        expect { cats_game.check_tie }.to change { cats_game.instance_variable_get(:@game_over) }.to(true)
+      end
+    end
+
+    context 'when the board is not full and the game is not won' do
+      before do
+        allow(game_board).to receive(:board_full?).and_return(false)
+        allow(cats_game).to receive(:game_won?).and_return(false)
+        allow(game_board).to receive(:display_board)
+      end
+
+      it 'keeps the state of game over as false' do
+        expect(cats_game.instance_variable_get(:@game_over)).to be(false)
+        cats_game.check_tie
+      end
+
+      it 'does not send game tie message' do
+        expect(cats_game).to_not receive(:tie_message)
+        cats_game.check_tie
+      end
+    end
+
+    context 'when the board is full and game is won' do
+    
+      before do
+        allow(game_board).to receive(:board_full?).and_return(true)
+        allow(cats_game).to receive(:game_won?).and_return(true)
+        allow(game_board).to receive(:display_board)
+      end
+
+      it 'does not execute tie message' do
+        expect(cats_game).to_not receive(:tie_message)
+        cats_game.check_tie
+      end
+    end
+  end
+
+  describe '#tie_message' do 
+
+    subject(:tie_game) { described_class.new(game_board) }
+    let(:game_board) { Board.new }
+
+    before do
+      allow(tie_game).to receive(:puts)
+    end
+
+
+    it 'sends display_board one time' do
+      expect(game_board).to receive(:display_board)
+      tie_game.tie_message
     end
   end
 end
